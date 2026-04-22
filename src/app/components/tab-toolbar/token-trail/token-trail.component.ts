27329ca180acd7b68550fa466cbb418e2f09f472
Alex <alexkolbfleisch@gmx.de>
2026-04-22 20:04:14 +0200

    Add Split view component to be able to adjust the side of left and right canvas in rg, proces-net and token trail tab

--- src/app/components/tab-toolbar/token-trail/token-trail.component.ts
+++ src/app/components/tab-toolbar/token-trail/token-trail.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TokenTrailDisplayComponent } from './token-trail-display/token-trail-display.component';
 import { TokenTrailDrawDisplayComponent } from './token-trail-draw-display/token-trail-draw-display';
+import { SplitViewComponent } from '../../split-view/split-view.component';
 
 @Component({
     selector: 'app-token-trail',
     standalone: true,
-    imports: [TokenTrailDisplayComponent, TokenTrailDrawDisplayComponent],
+    imports: [TokenTrailDisplayComponent, TokenTrailDrawDisplayComponent, SplitViewComponent],
     templateUrl: './token-trail.component.html',
     styleUrl: './token-trail.component.css',
 })
